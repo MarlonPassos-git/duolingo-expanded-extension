9e6d3fdbb9e4a1c5f8bd710cc348494cde336c29
marlonpassos <marlon.passos@cadastra.com>
2024-09-10 01:32:55 +0000

    docs: explica sobre ids

--- manifest.config.ts
+++ manifest.config.ts
@@ -2,6 +2,8 @@ import type { ConfigEnv } from 'vite'
 import { defineManifest } from '@crxjs/vite-plugin'
 import packageJson from './package.json'
 
+/** @see https://developer.chrome.com/docs/extensions/reference/manifest/key#keep-consistent-id */
+const EXTENSION_KEY_HASH = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0FAp+xWiJpGBmsKPhGcqF4/gQN9F5tmXgEVYEHUHc8HcBIUcT+9w+jo4q2OtXa2ThqgEXsx2zcNZIWJ/5yXcofVry5E2/HKBuLWHNtYOlI1rhwc/CLujo0RHhzF7rIiYcMPQdBhzr6L0u5u9N29VUWjLozltquKRcUbjXNe4LT7+q/akhn5tvfvWHkQ9qC6mRjvGwGTFlh1A6+vWKKSVYx/J+IBHW+I2X5NlAxwG734OMYVWRWK487jf1wsWZ2jHRTqg9TB3htT+84r7+E3kFYMycow9+2EhvoI2k5VGhZw1tAJcpie1Poozc5u8CTrZ4sZ5LK4h59OCOxmejC048QIDAQAB'
 const { version } = packageJson
 
 export default defineManifest(async (env) => {
@@ -31,7 +33,11 @@ export default defineManifest(async (env) => {
     name: isDev(env) ? '[Dev] Duolingo expanded' : 'Duolingo expanded',
     version: getVersion(),
     version_name: version,
-    key: isDev(env) ? 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0FAp+xWiJpGBmsKPhGcqF4/gQN9F5tmXgEVYEHUHc8HcBIUcT+9w+jo4q2OtXa2ThqgEXsx2zcNZIWJ/5yXcofVry5E2/HKBuLWHNtYOlI1rhwc/CLujo0RHhzF7rIiYcMPQdBhzr6L0u5u9N29VUWjLozltquKRcUbjXNe4LT7+q/akhn5tvfvWHkQ9qC6mRjvGwGTFlh1A6+vWKKSVYx/J+IBHW+I2X5NlAxwG734OMYVWRWK487jf1wsWZ2jHRTqg9TB3htT+84r7+E3kFYMycow9+2EhvoI2k5VGhZw1tAJcpie1Poozc5u8CTrZ4sZ5LK4h59OCOxmejC048QIDAQAB' : undefined,
+    /**
+     * ensures that the browser will always use the same id for the extension.
+     * This is especially useful for automated testing.
+     */
+    key: isDev(env) ? EXTENSION_KEY_HASH : undefined,
   }
 })
 
